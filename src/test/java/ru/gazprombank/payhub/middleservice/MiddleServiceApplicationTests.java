65317cb81ceeb74869f13316a752bc4424bdeef1
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2024-06-03 16:23:39 +0300

    Создал базовый проект

---
+++ src/test/java/ru/gazprombank/payhub/middleservice/MiddleServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.gazprombank.payhub.middleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
